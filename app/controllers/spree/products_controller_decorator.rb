eeabbcafc140f3db32e407cfacf8573c8f13dc86
umairrazam <omairrr.azam@gmail.com>
2025-09-03 18:52:13 +0500

    design adjustments

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -4,6 +4,19 @@ module Spree
       base.helper Spree::ReviewsHelper
     end
 
+    def reviews
+      @product = Spree::Product.friendly.find(params[:product_id])
+      respond_to do |format|
+        format.html do
+          render inline: <<~HTML
+            <turbo-frame id="reviews_frame">
+              <%= render "spree/shared/reviews", product: @product %>
+            </turbo-frame>
+          HTML
+        end
+      end
+    end
+
     ::Spree::ProductsController.prepend self if ::Spree::Core::Engine.frontend_available? && ::Spree::ProductsController.included_modules.exclude?(self)
   end
 end
